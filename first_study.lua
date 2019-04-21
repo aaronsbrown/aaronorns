e6927193bbefad92e19f8944f724b9817b9b986e
Aaron Brown <aaron.s.brown@gmail.com>
2019-04-21 11:07:11 +0700

    first commit

---
+++ first_study.lua
@@ -0,0 +1,44 @@
+-- many tomorrows
+-- norns study 1
+
+engine.name = "TestSine"
+
+function init()
+  position = 0
+  volume = 0
+  engine.hz(333)
+  engine.amp(.1)
+  print("the end and the beginning they are the same.")
+end
+
+function key(n,z)
+  if n == 3 then
+    engine.hz(100 + (100 * z*math.random(10))/6)
+  end
+  
+  if n == 2 then
+    engine.hz(600 + 100 * z*math.random(10))
+  end
+end
+
+function enc(n,d)
+  print("encoder " .. n .. " == " .. d)
+end
+
+function enc(n,d)
+  if n == 3 then
+    position = position + d
+    print("encoder 3 at position: " .. position)
+  end
+ 
+  if n == 2 then
+    volume = volume + d*.1
+    print("volume = " .. volume )
+    if volume <= 0 then
+      volume = 0 
+    else     
+      engine.amp( volume )
+    end
+  end
+    
+end
